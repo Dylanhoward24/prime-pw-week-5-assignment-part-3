7586045de048a17c5f11d3f19d26910dcd048367
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-11 14:32:41 -0500

    corrected functions for adding albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,9 +12,10 @@ function addToCollection (title, artist, yearPublished){
   return album;
 }
 
-console.log('Added albumOne', collection.albumOne = addToCollection('Purple Rain', 'Prince', '1984'));
-console.log('Added albumTwo', collection.albumTwo = addToCollection('Enema of the State', 'Blink 182', '1999'));
-console.log('Added albumThree', collection.albumThree = addToCollection('No Strings Attached', 'NSYNC', '2000'));
-console.log('Added albumFour', collection.albumFour = addToCollection('Abbey Road', 'The Beatles', '1969'));
-console.log('Added albumFive', collection.albumFive = addToCollection('Led Zeppelin IV', 'Led Zeppelin', '1971'));
-console.log('Added albumSix', collection.albumSix = addToCollection('Inside', 'Bo Burnham', '2021'));
+console.log('Added first album', addToCollection('Purple Rain', 'Prince', '1984'));
+console.log('Added second album', addToCollection('Enema of the State', 'Blink 182', '1999'));
+console.log('Added third album', addToCollection('No Strings Attached', 'NSYNC', '2000'));
+console.log('Added fourth album', addToCollection('Abbey Road', 'The Beatles', '1969'));
+console.log('Added fifth album', addToCollection('Led Zeppelin IV', 'Led Zeppelin', '1971'));
+console.log('Added sixth album', addToCollection('Inside', 'Bo Burnham', '2021'));
+console.log(collection);
