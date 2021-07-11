335dd3328332c5ec5986a097c94fc231c0826ec9
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-11 14:29:00 -0500

    added six albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,4 +12,9 @@ function addToCollection (title, artist, yearPublished){
   return album;
 }
 
-addToCollection('Purple Rain', 'Prince', '1984');
+console.log('Added albumOne', collection.albumOne = addToCollection('Purple Rain', 'Prince', '1984'));
+console.log('Added albumTwo', collection.albumTwo = addToCollection('Enema of the State', 'Blink 182', '1999'));
+console.log('Added albumThree', collection.albumThree = addToCollection('No Strings Attached', 'NSYNC', '2000'));
+console.log('Added albumFour', collection.albumFour = addToCollection('Abbey Road', 'The Beatles', '1969'));
+console.log('Added albumFive', collection.albumFive = addToCollection('Led Zeppelin IV', 'Led Zeppelin', '1971'));
+console.log('Added albumSix', collection.albumSix = addToCollection('Inside', 'Bo Burnham', '2021'));
