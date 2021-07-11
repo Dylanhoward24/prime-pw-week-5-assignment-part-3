f321b9b9f113ff02c7a6a1a0ee089204897da4e4
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-11 14:07:40 -0500

    added function addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,15 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection (title, artist, yearPublished){
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  } // end album
+  collection.push(album);
+  return album;
+}
+
+addToCollection('Purple Rain', 'Prince', '1984');
